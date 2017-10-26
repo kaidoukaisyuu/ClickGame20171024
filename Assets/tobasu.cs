b07a3d85e238419622799281e3070f07a229ad61
GT20176039\user <nkc.kaisyuu.kousaka@gmail.com>
2017-10-26 11:00:52 +0900

    消えるとこまで

---
+++ Assets/tobasu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tobasu : MonoBehaviour {
+
+	// Use this for initialization
+    public void shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    GameObject Sphere;
+    void OnCollisionEnter(Collision other)
+    {
+        this.Sphere = GameObject.Find("Sphere");
+        Destroy(Sphere);
+    }
+    
+	void Start () {
+        //shoot(new Vector3(0, 200, 2000));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
